175263a8fe91a4d312b4a86ec543c7ac494f80eb
CroissantToes <CroissantToes@users.noreply.github.com>
2020-02-14 20:54:28 -0500

    This is it

---
+++ Tutorial 1.5/Assets/Scripts/CatController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatController : MonoBehaviour
+{
+    public AudioSource source;
+    public AudioClip clipOne;
+    public AudioClip clipTwo;
+
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            source.clip = clipOne;
+            source.Play();
+            anim.SetInteger("State", 1);
+        }
+
+        if(Input.GetKeyUp(KeyCode.W))
+        {
+            source.Stop();
+            anim.SetInteger("State", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            source.clip = clipTwo;
+            source.Play();
+            anim.SetInteger("State", 2);
+        }
+
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            source.Stop();
+            anim.SetInteger("State", 0);
+        }
+
+        if(Input.GetKeyDown(KeyCode.L))
+        {
+            source.loop = true;
+        }
+
+        if(Input.GetKeyUp(KeyCode.L))
+        {
+            source.loop = false;
+        }
+
+        if (Input.GetKey("escape"))
+            Application.Quit();
+
+    }
+}
